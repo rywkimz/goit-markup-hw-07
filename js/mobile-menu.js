881a977f1068dcf51f7403ab3b11971668dbd6ab
Georgiana <fil.georgiana03@gmail.com>
2024-05-24 18:10:45 +0300

    update

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,28 +1,21 @@
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const menuToggleBtn = document.querySelector('.js-menu-toggle');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    const isMenuOpen = mobileMenu.classList.contains('is-open');
+    mobileMenu.classList.toggle('is-open', !isMenuOpen);
+    menuToggleBtn.setAttribute('aria-expanded', !isMenuOpen);
+    bodyScrollLock.toggleBodyScroll(!isMenuOpen, mobileMenu);
   };
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
+  menuToggleBtn.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
+    menuToggleBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
